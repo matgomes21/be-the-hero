a53722119bb93cb99c6708faf2d7a16f4fdf6e99
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-06-25 14:51:34 -0300

    Created tables on database with knex and sqlite3

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({
-        message: 'Hello World'
-    });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
