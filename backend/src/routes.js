1ee4e520b97124fb36b74e7d535cbf00033e72dc
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-06-25 15:06:03 -0300

    Created Controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,21 @@
-const express = require('express');
+const express = require('express')
 
-const routes = express.Router();
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
 
-routes.post('/users', (request, response) => {
-    const body = request.body;
+const routes = express.Router()
 
-    console.log(body);
+routes.post('/sessions', SessionController.create)
 
-    return response.json({
-        message: 'Hello World',
-    });
-});
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
 
-module.exports = routes;
+routes.get('/profile', ProfileController.index)
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
+
+module.exports = routes
